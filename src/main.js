2bc74be33d1eb5e36368c9468a4b06d0f14eb603
vaishnav-sh <vaishnavshyamwork@gmail.com>
2021-12-22 16:50:49 +0530

    added count and burger animations

--- src/main.js
+++ src/main.js
@@ -1,21 +1,47 @@
+// Burger menu animation
 const nav = document.querySelector('.nav');
 const navList = document.querySelectorAll('.nav-item');
 const burger = document.querySelector('.burger');
-// menu animation
-burger.addEventListener('click', () => {
+
+burger.addEventListener('click', showNavigation)
+navList.forEach(navListItem => {
+    navListItem.addEventListener('click', showNavigation);
+});
+
+// show or hide navigation links
+function showNavigation() {
     if(!nav.classList.contains('show-nav')) {
         nav.classList.add('show-nav')
     } else {
         nav.classList.remove('show-nav')
     }
+
+    burger.classList.toggle('toggle')
+}
+
+// counter section animation
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 850) {
+        startCount();
+    }
 })
 
-navList.forEach(navListItem => {
-    navListItem.addEventListener('click', () => {
-        if(!nav.classList.contains('show-nav')) {
-            nav.classList.add('show-nav')
-        } else {
-            nav.classList.remove('show-nav')
+function startCount() {
+    const counters = document.querySelectorAll('.counter')
+    const speed = 30;
+    counters.forEach(counter => {
+        function updateCount() {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText.match(/\d+/);
+            const inc = Math.round(target / speed);
+    
+            if(count < target) {
+                counter.innerText = (count + inc) + "+";
+                setTimeout(updateCount, 50);
+            } else {
+                counter.innerText = target + "+";
+            }
         }
+        updateCount();
     });
-});
+}
